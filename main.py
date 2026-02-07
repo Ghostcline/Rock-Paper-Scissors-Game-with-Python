157700e520b57c3241c5258abc31b53a930e5375
abdualrhman-ai <dhwwwmalmlk@gmail.com>
2026-02-07 19:40:39 +0800

    Rock, Paper, Scissors Game project with Python

---
+++ main.py
@@ -0,0 +1,41 @@
+from game_logic import determine_winner
+from player import get_user_input
+from computer import get_computer_choice
+
+EMOJI_MAP = {"rock" : "✊", "paper" :"👋","scissors" : "✌️"}
+valid_choices = ["rock", "paper", "scissors"]
+
+def get_emoji(choice):
+    return EMOJI_MAP.get(choice)
+
+def welcome_user():
+    print("Welcome to Rock, Paper, Scissors!")
+ 
+welcome_user()
+
+player_score = 0
+computer_score = 0
+play_again = "yes"
+while play_again == "yes":
+
+    user_choice = get_user_input(valid_choices)
+
+    print(f"You chose: {get_emoji(user_choice)}")
+
+    computer_choice = get_computer_choice(valid_choices)
+
+    print(f"Computer chose: {get_emoji(computer_choice)}")
+
+    result = determine_winner(user_choice, computer_choice)
+
+    print(result)
+
+    if result == "You win!":
+        player_score += 1
+    elif result == "You lose!":
+        computer_score += 1
+    
+    play_again = input("Do you want to play again? (yes/no): ")
+print(f"Your score: {player_score}")
+print(f"Computer score: {computer_score}")
+print("Thank you for playing")
